66ee11540c09665c0a802c637cc68d5416deff42
Jayesh Mahale <68965428+IMjayesh45@users.noreply.github.com>
2024-10-23 15:03:55 +0000

    container config done

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -2,7 +2,13 @@ const accountId = 1
 let accountEmail = "jackie@google.com"
 var accountPassword = "123456"
 accountCity = "Jalgaon"
+let accountState;
 
-accountId = 2
+
+accountEmail = "jackie17@gmail.com"
+accountPassword = "12345555"
+accountCity = " Mumbai"
 
 console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as new version
+// alert(3+3) we are using node js not browser
+
+
+let name = "Jayesh"
+let age = 22
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false
+// null = standalone value 
+// indefined => 
+// symbol > uniqueness 
+
+// object 
+
+
+console.log(typeof "Jayesh");
